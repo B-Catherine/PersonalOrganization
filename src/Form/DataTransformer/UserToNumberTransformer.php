3baaf978f5542d4c5b8ebea0e5fa3a720e3e62ea
Sébastien Prodhomme <sprodhomme133@gmail.com>
2020-04-15 16:56:30 +0100

    Ajout des DataTransformers sur PersonalCalendar et User

---
+++ src/Form/DataTransformer/UserToNumberTransformer.php
@@ -0,0 +1,66 @@
+<?php
+// src/Form/DataTransformer/UserToNumberTransformer.php
+namespace App\Form\DataTransformer;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+class UserToNumberTransformer implements DataTransformerInterface
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * Transforms an object (issue) to a string (number).
+     *
+     * @param  User|null $user
+     * @return string
+     */
+    public function transform($user)
+    {
+        if (null === $user) {
+            return '';
+        }
+
+        return $user->getId();
+    }
+
+    /**
+     * Transforms a string (number) to an object (User).
+     *
+     * @param  string $userNumber
+     * @return User|null
+     * @throws TransformationFailedException if object (User) is not found.
+     */
+    public function reverseTransform($userNumber)
+    {
+        // no issue number? It's optional, so that's ok
+        if (!$userNumber) {
+            return;
+        }
+
+        $user = $this->entityManager
+            ->getRepository(User::class)
+            // query for the issue with this id
+            ->find($userNumber)
+        ;
+
+        if (null === $user) {
+            // causes a validation error
+            // this message is not shown to the user
+            // see the invalid_message option
+            throw new TransformationFailedException(sprintf(
+                'An user with number "%s" does not exist!',
+                $userNumber
+            ));
+        }
+
+        return $user;
+    }
+}
